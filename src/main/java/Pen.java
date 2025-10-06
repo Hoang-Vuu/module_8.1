d45509cedf21e993c9e93be9d1bc6af7c1ddf7b3
Vu Dang Hoang <vdhoang240706@gmail.com>
2025-10-06 10:07:38 +0300

    c

--- src/main/java/Pen.java
+++ src/main/java/Pen.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Pen {
+    public enum Color {
+        RED("red"), GREEN("green"), BLUE("blue");
+        private final String color;
+        Color(String color) { this.color = color; }
+        @Override public String toString() { return color; }
+    }
+
+    private Color color;
+    private boolean isCapped;
+
+    public Pen() {
+        this.color = Color.RED;
+        this.isCapped = true;
+    }
+
+    public Pen(Color color) {
+        this.color = color;
+        this.isCapped = true;
+    }
+
+    public void capOn() {
+        isCapped = true;
+    }
+
+    public void capOff() {
+        isCapped = false;
+    }
+
+    public String draw() {
+        return isCapped ? "" : "Drawing " + color.toString();
+    }
+
+    public void changeColor(Color newColor) {
+        if (isCapped) {
+            this.color = newColor;
+        }
+    }
 }
